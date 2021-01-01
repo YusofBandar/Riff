cf0d2d1f74d747c0e2c1175bbfa0d150313c209b
YusofBandar <yusof.bandar@squaredup.com>
2021-01-01 23:04:22 +0000

    Adds viewport to compare function. The viewport allows differently sized images to be compared as well as comparing specific regions within a image.

--- src/bin/riff.rs
+++ src/bin/riff.rs
@@ -10,8 +10,9 @@ fn main() {
 
     // golden-rod!!
     let diff_colour = [218 as u8, 165 as u8, 32 as u8, 255 as u8];
+    let view_port = [0, 0, 1000, 419];
 
-    let img = compare(&base_img, &diff_img, diff_colour);
+    let img = compare(&base_img, &diff_img, diff_colour, view_port);
     img.save("diff.png").unwrap();
 }
 
@@ -19,10 +20,11 @@ fn read_image_from_file(path: &String) -> image::DynamicImage {
     image::open(path).unwrap()
 }
 
-fn compare (base: &image::DynamicImage, diff: &image::DynamicImage, diff_colour: [u8; 4]) -> image::RgbaImage {
+fn compare (base: &image::DynamicImage, diff: &image::DynamicImage, diff_colour: [u8; 4], view_port: [u32; 4]) -> image::RgbaImage {
     let base = base.to_rgba8();
     let diff = diff.to_rgba8();
     
+    let view_port = [view_port[0], view_port[1], view_port[2] - 1, view_port[3] - 1];
     let max_delta = (35215 as f64) * 0.1 * 0.1;
 
     let (width, height) = base.dimensions();
@@ -30,17 +32,31 @@ fn compare (base: &image::DynamicImage, diff: &image::DynamicImage, diff_colour:
     let mut diff_image: image::RgbaImage = image::ImageBuffer::new(width, height);
 
     for (x, y, base_pixel) in base.enumerate_pixels() {
+        if is_past_viewport(x, y, view_port) {
+            break;
+        }
 
-        let diff_pixel = diff.get_pixel(x, y);
+        if is_within_viewport(x, y, view_port) {
+            let diff_pixel = diff.get_pixel(x, y);
 
-        // if pixels are the same don't bother to calculate delta
-        if base_pixel[0] != diff_pixel[0] || base_pixel[1] != diff_pixel[1] || base_pixel[2] != diff_pixel[2] || base_pixel[3] != diff_pixel[3] {
-            let delta = calculate_pixel_colour_delta(base_pixel, diff_pixel);
-            if delta > max_delta {
-                diff_image.put_pixel(x, y, Rgba(diff_colour));
+            // if pixels are the same don't bother to calculate delta
+            if base_pixel[0] != diff_pixel[0] || base_pixel[1] != diff_pixel[1] || base_pixel[2] != diff_pixel[2] || base_pixel[3] != diff_pixel[3] {
+                let delta = calculate_pixel_colour_delta(base_pixel, diff_pixel);
+                if delta > max_delta {
+                    diff_image.put_pixel(x, y, Rgba(diff_colour));
+                }
             }
         }
     }
 
     diff_image
+}
+
+fn is_past_viewport(x: u32, y: u32, view_port: [u32; 4]) -> bool {
+    x >= view_port[0] + view_port[2] && y > view_port[1] + view_port[3]
+}
+
+fn is_within_viewport(x: u32, y: u32, view_port: [u32; 4]) -> bool {
+    x >= view_port[0] && x <= view_port[0] + view_port[2] &&
+     y >= view_port[1] && y <= view_port[3]
 }

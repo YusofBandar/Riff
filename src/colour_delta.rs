2c4ca704354da1778bc9fc392579cca89592e432
YusofBandar <yusof.bandar@squaredup.com>
2021-01-02 22:37:16 +0000

    Makes all functions within riff colour delta module public

--- src/colour_delta.rs
+++ src/colour_delta.rs
@@ -1,22 +1,22 @@
 use image::{Rgba, Primitive};
 
-fn blend(colour: &f64, alpha: &f64) -> f64 {
+pub fn blend(colour: &f64, alpha: &f64) -> f64 {
     (255 as f64) + (colour - (255 as f64)) * alpha
 }
 
-fn rgb2y(r: &f64, g: &f64, b: &f64) -> f64 {
+pub fn rgb2y(r: &f64, g: &f64, b: &f64) -> f64 {
     r * 0.29889531 + g * 0.58662247 + b * 0.11448223
 }
 
-fn rgb2i(r: &f64, g: &f64, b: &f64) -> f64 {
+pub fn rgb2i(r: &f64, g: &f64, b: &f64) -> f64 {
     r * 0.59597799 - g * 0.27417610 - b * 0.32180189
 }
 
-fn rgb2q(r: &f64, g: &f64, b: &f64) -> f64 {
+pub fn rgb2q(r: &f64, g: &f64, b: &f64) -> f64 {
     r * 0.21147017 - g * 0.52261711 + b * 0.31114694
 }
 
-fn blend_semi_transparent_colour(r: &f64, g: &f64, b: &f64, a: &f64) -> [f64; 4] {
+pub fn blend_semi_transparent_colour(r: &f64, g: &f64, b: &f64, a: &f64) -> [f64; 4] {
     if a < &(255 as f64) {
         [blend(&r, &a), blend(&g, &a), blend(&b, &a), a / 255 as f64]
     } else {
@@ -24,7 +24,7 @@ fn blend_semi_transparent_colour(r: &f64, g: &f64, b: &f64, a: &f64) -> [f64; 4]
     }
 }
 
-fn rgba_to_f64<T: Primitive>(r: &T, g: &T, b: &T, a: &T) -> [f64; 4] {
+pub fn rgba_to_f64<T: Primitive>(r: &T, g: &T, b: &T, a: &T) -> [f64; 4] {
     let r_64  = match r.to_f64() {
         Some(val) => val,
         None => 0 as f64

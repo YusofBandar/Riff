81503b2cfb68893baa7464ce01519352315a2066
YusofBandar <yusof.bandar@squaredup.com>
2021-01-04 12:22:37 +0000

    Adds custom parser to parse diff colour and view port arguments

--- README.md
+++ README.md
@@ -15,4 +15,7 @@ riff <base_path> <diff_path> <output_path>
 ```
 --alpha          Blending value of unchaged pixels, 0 alpha disables drawing of base image [default :0]
 --threshold      Matching threshold, smaller values makes pixel comparison more sensitive [default: 0.1]
+--diffColour     The color of differing pixels in [R, G, B, A] format [default: [218, 165, 32, 255]]
+--viewPort       The region within base image to compare to in [x, y, width, height] format. Useful when comparing differently sized images
+
 ```
--- src/bin/riff.rs
+++ src/bin/riff.rs
@@ -13,13 +13,34 @@ struct Opt {
     /// Path to output image (jpeg or png)
     output_path: String,
 
+    /// The color of differing pixels in [R, G, B, A] format
+    #[structopt(long="diffColour", default_value="[218, 165, 32, 255]", parse(try_from_str = "parse_num_array"))]
+    diff_colour: [u32; 4],
+
     /// Matching threshold, smaller values makes pixel comparison more sensitive
     #[structopt(long = "threshold", default_value="0.1")]
     threshold: f64,
 
     /// Blending value of unchaged pixels, 0 alpha disables drawing of base image
     #[structopt(long = "alpha", default_value="0")]
-    alpha: f64
+    alpha: f64,
+
+    /// The region within base image to compare to in [x, y, width, height] format. Useful when comparing differently sized images
+    #[structopt(long="viewPort", parse(try_from_str = "parse_num_array"))]
+    view_port: Option<[u32; 4]>
+}
+
+fn parse_num_array(array: &str) -> Result<[u32; 4], &'static str> {
+    let array = array.trim_start_matches("[")
+        .trim_end_matches("]")
+        .split(",");
+
+    let mut num_array: [u32; 4] = [255; 4];
+    for (i, el) in array.enumerate() {
+        num_array[i] = el.trim().parse::<u32>().unwrap();
+    }
+
+    Ok(num_array)
 }
 
 fn main() {
@@ -31,26 +52,30 @@ fn main() {
     let base_img = read_image_from_file(&base);
     let diff_img = read_image_from_file(&diff);
 
-    // golden-rod!!
-    let diff_colour = [218 as u8, 165 as u8, 32 as u8, 255 as u8];
-    let view_port = [0, 0, 1000, 419];
 
-    let img = compare(&base_img, &diff_img, opt.threshold, opt.alpha, diff_colour, view_port);
+    let img = compare(&base_img, &diff_img, opt.threshold, opt.alpha, opt.diff_colour, opt.view_port);
     img.save(opt.output_path).unwrap();
+
 }
 
 fn read_image_from_file(path: &String) -> image::DynamicImage {
     image::open(path).unwrap()
 }
 
-fn compare (base: &image::DynamicImage, diff: &image::DynamicImage, threshold: f64, alpha: f64, diff_colour: [u8; 4], view_port: [u32; 4]) -> image::RgbaImage {
+fn compare (base: &image::DynamicImage, diff: &image::DynamicImage, threshold: f64, alpha: f64, diff_colour: [u32; 4], view_port: Option<[u32; 4]>) -> image::RgbaImage {
     let base = base.to_rgba8();
     let diff = diff.to_rgba8();
+
+    let (width, height) = base.dimensions();
+
+    let view_port = match view_port {
+        Some(view) => [view[0], view[1], view[2] - 1, view[3] - 1],
+        None => [0, 0, width-1, height-1]
+    };
     
     let view_port = [view_port[0], view_port[1], view_port[2] - 1, view_port[3] - 1];
     let max_delta = (35215 as f64) * threshold * threshold;
 
-    let (width, height) = base.dimensions();
 
     let mut diff_image: image::RgbaImage = image::ImageBuffer::new(width, height);
 
@@ -66,9 +91,9 @@ fn compare (base: &image::DynamicImage, diff: &image::DynamicImage, threshold: f
             if base_pixel[0] != diff_pixel[0] || base_pixel[1] != diff_pixel[1] || base_pixel[2] != diff_pixel[2] || base_pixel[3] != diff_pixel[3] {
                 let delta = calculate_pixel_colour_delta(base_pixel, diff_pixel);
                 if delta > max_delta {
-                    diff_image.put_pixel(x, y, Rgba(diff_colour));
+                    diff_image.put_pixel(x, y, Rgba([diff_colour[0] as u8, diff_colour[1] as u8, diff_colour[2] as u8, diff_colour[3] as u8]));
                 }
-            }else if alpha > 0.0  {
+            } else if alpha > 0.0  {
                 // draw unchaged pixels with specified alpha
                 let pixel = rgba_to_f64(&base_pixel[0], &base_pixel[1], &base_pixel[2], &base_pixel[3]);
                 let colour = rgb2y(&pixel[0], &pixel[1], &pixel[2]);
